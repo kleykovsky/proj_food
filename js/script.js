bdcf049c58a0ad80abdee6cc33ded62430174f89
Alex <kleykovsky@gmail.com>
2022-08-10 15:47:05 +0500

    rev:2.0

--- js/script.js
+++ js/script.js
@@ -159,7 +159,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
-			this.classList = classes;
+			this.classes = classes;
 			this.parent = document.querySelector(parentSelector);
 			this.transfer = 10500;
 			this.changeToUZS();
@@ -169,24 +169,23 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 		render() {
 			const element = document.createElement('div');
-
-			if (this.classes.length ===0) {
+			if (this.classes === 0) {
 				this.element = 'menu__item';
 				element.classList.add(this.element);
 			} else {
-				this.classList.forEach(className => element.classList.add(className));
+				this.classes.forEach(className => element.classList.add(className));
 			}
-
 			element.innerHTML = `
-					<img src=${this.src} alt=${this.alt}>
-					<h3 class="menu__item-subtitle">${this.title}</h3>
-					<div class="menu__item-descr">${this.descr}</div>
-					<div class="menu__item-divider"></div>
-					<div class="menu__item-price">
-						<div class="menu__item-cost">Цена:</div>
-						<div class="menu__item-total"><span>${this.price}</span> uzs/день </div>
-					</div>
-					`;
+
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> uzs/день </div>
+				</div>
+			`;
 			this.parent.append(element);
 		}
 	}
@@ -197,10 +196,11 @@ window.addEventListener('DOMContentLoaded', () => {
 		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 		9,
 		'.menu .container',
-		// 'menu__item',
+		'menu__item'
 		// 'big'
 	);
 	div.render();
+
 	const div2 = new MenuCard(
 		"img/tabs/elite.jpg",
 		"elite",
@@ -211,6 +211,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		'menu__item'
 	);
 	div2.render();
+
 	const div3 = new MenuCard(
 		"img/tabs/post.jpg",
 		"post",
@@ -221,14 +222,17 @@ window.addEventListener('DOMContentLoaded', () => {
 		'menu__item'
 	);
 	div3.render();
+
 	// new MenuCard(
 	// 	"img/tabs/vegy.jpg",
 	// 	"vegy",
 	// 	"Меню 'Фитнес'",
 	// 	'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 	// 	9,
-	// 	'.menu .container'
-	// ).render();
+	// 	'.menu .container',
+	// 	'big'
+	// 	).render();
+
 	// new MenuCard(
 	// 	"img/tabs/elite.jpg",
 	// 	"elite",
@@ -237,6 +241,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	// 	14,
 	// 	'.menu .container'
 	// ).render();
+
 	// new MenuCard(
 	// 	"img/tabs/post.jpg",
 	// 	"post",
@@ -305,3 +310,30 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // console.log(document.documentElement.scrollTop);
 //////////////////////////////////////////////////
+
+// new MenuCard(
+	// 	"img/tabs/vegy.jpg",
+	// 	"vegy",
+	// 	"Меню 'Фитнес'",
+	// 	'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+	// 	9,
+	// 	'.menu .container'
+	// ).render();
+
+	// new MenuCard(
+	// 	"img/tabs/elite.jpg",
+	// 	"elite",
+	// 	"Меню “Премиум”",
+	// 	'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+	// 	14,
+	// 	'.menu .container'
+	// ).render();
+
+	// new MenuCard(
+	// 	"img/tabs/post.jpg",
+	// 	"post",
+	// 	"Меню 'Постное'",
+	// 	'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+	// 	21,
+	// 	'.menu .container'
+	// ).render();
