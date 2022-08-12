7d2eef52975695f1b0c97a46b85d81071b968a83
Alex <kleykovsky@gmail.com>
2022-08-12 23:27:04 +0500

    rev: 2.0

--- js/script.js
+++ js/script.js
@@ -176,10 +176,6 @@ window.addEventListener('DOMContentLoaded', () => {
 				this.classes.forEach(className => element.classList.add(className));
 			}
 			element.innerHTML = `
-<<<<<<< HEAD
-=======
-
->>>>>>> 3553265e7f3aa7a11db89af0f71c193851dd6ca9
 				<img src=${this.src} alt=${this.alt}>
 				<h3 class="menu__item-subtitle">${this.title}</h3>
 				<div class="menu__item-descr">${this.descr}</div>
@@ -341,4 +337,3 @@ window.addEventListener('DOMContentLoaded', () => {
 	// 	21,
 	// 	'.menu .container'
 	// ).render();
-	
