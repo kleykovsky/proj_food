579eeb9184b70b4b117c1854b2ee980df585ae35
Alex <kleykovsky@gmail.com>
2022-08-13 20:04:59 +0500

    rev: 2.5

--- js/script.js
+++ js/script.js
@@ -248,7 +248,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			const request = XMLHttpRequest();
 			request.open('POST', 'server.php');
 
-			request.setRuquestHeader('COntent-type', 'multipart/form-data');
+			request.setRequestHeader('Content-type', 'multipart/form-data');
 			const formData = new FormData(form);
 
 			request.send(formData);
