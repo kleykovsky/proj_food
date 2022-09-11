78eed7025149011b2a7dd34153d42c243cfec11e
Alex <kleykovsky@gmail.com>
2022-09-12 01:52:04 +0500

    rev: 3.6

--- js/script.js
+++ js/script.js
@@ -430,13 +430,19 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 	dotsNavigation();
 
+
+	function deleteNotDigits() {
+		let str;
+		return +str.replace(/\D/g, '');
+	}
+
 	function buttonNavigationSliders() {
 		// button right
 		next.addEventListener('click', () => {
-			if(offset === +width.replace(/\D/g, '') * (slides.length - 1)){
+			if(offset === deleteNotDigits(width) * (slides.length - 1)){
 				offset = 0;
 			} else {
-				offset += +width.replace(/\D/g, '');
+				offset += deleteNotDigits(width);
 			}
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -458,9 +464,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		// button left
 		prev.addEventListener('click', () => {
 			if(offset === 0){
-				offset = +width.replace(/\D/g, '') * (slides.length - 1);
+				offset = deleteNotDigits(width) * (slides.length - 1);
 			} else {
-				offset -= +width.replace(/\D/g, '');
+				offset -= deleteNotDigits(width);
 			}
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -487,7 +493,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		dot.addEventListener('click', (e) => {
 			const slideTo = e.target.getAttribute('data-slide-to');
 			slideIndex = slideTo;
-			offset = +width.replace(/\D/g, '') * (slideTo - 1);
+			offset = deleteNotDigits(width) * (slideTo - 1);
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
 			if(slides.length < 10) {
